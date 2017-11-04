6ff49653f76fb8b8569ef2cb33e6394844c522b9
n8dgr8 <nathan@kwintek.net>
2017-11-03 23:20:24 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import
+  Flask,
+  redirect,
+  render_template,
+  request,
+  url_for
+import redis
+import short_url
+
+app = Flask(__name__)
+
+app.redis_client = redis.StrictRedis(
+  host='localhost',
+  port=6379,
+  db=0
+)
+
+@app.route('/')
+def index():
+  return 'Yup'
+
+@app.route('/chart', methods=['POST'])
+def create_chart():
+  new_chart_id = app.redis_client.incr('chart:chart_id')
+  encoded_new_chart_id = short_url.encode_url(new_chart_id)
+  app.redis_client.set('chart:%s:data' % encoded_new_chart_id, request.values['data'])
+  return redirect(url_for('get_chart', chart_id=encoded_new_chart_id))
+
+@app.route('/chart/<chart_id>', methods=['GET'])
+def get_chart(chart_id=None):
+  chart_data = app.redis_client.get('chart:%s:data' % chart_id)
+  template_data = {
+    'chart_id': chart_id,
+    'chart_data': chart_data 
+  }
+  return render_template('chart.html', template_data=template_data)
